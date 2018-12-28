26cae8799816ac95671e44d5db86befa46155688
Evgeniy Beletskiy <mbgroup1988@gmail.com>
2018-12-28 17:22:53 +0300

    test fix

--- yandex.js
+++ yandex.js
@@ -11,38 +11,31 @@ describe("Yandex tests", function() {
 
     it("Location London", function() {
         browser.waitForAngularEnabled(false);
+        browser.manage().timeouts().implicitlyWait(4000);
         browser.get("https://yandex.ru/");
 
         // Переключение на Лондон
         element(by.css(".geolink > .geolink__reg")).click();
         element(by.css(".input__control.input__input")).clear();
         element(by.css(".input__control.input__input")).sendKeys("Лондон");
-        var EC = protractor.ExpectedConditions;
-        var popUpLondon = element(by.css('.popup__items.input__popup-items > li:first-child'));
-        browser.wait(EC.elementToBeClickable(popUpLondon), 6000);
-        popUpLondon.click();
+        element(by.css('.popup__items.input__popup-items > li:first-child')).click();
 
         //Сохранение значений для Лондона 
-        var pageElse = element(by.css(".home-tabs__more-switcher.dropdown2__switcher"));
-        browser.wait(EC.elementToBeClickable(pageElse), 6000);
-        pageElse.click();
-        var elseLondon = element.all(by.css(".home-tabs__more-switcher.dropdown2__switcher")).last().getText();
+        element(by.css(".home-tabs__more-switcher.dropdown2__switcher")).click();
+        var saveValueLondon = element.all(by.css(".home-tabs__more-switcher.dropdown2__switcher")).last().getText();
 
         // Переключение на Париж
         element(by.css(".geolink__reg")).click();
         element(by.css(".input__control.input__input")).clear();
         element(by.css(".input__control.input__input")).sendKeys("Париж");
         browser.sleep('1000');
-        var popUpParis = element(by.css('.popup__items.input__popup-items > li:first-child'));
-        browser.wait(EC.elementToBeClickable(popUpParis), 6000);
-        popUpParis.click();
+        element(by.css('.popup__items.input__popup-items > li:first-child')).click();
 
         //Сохранение значения для Парижа
-        browser.wait(EC.elementToBeClickable(pageElse), 6000);
-        pageElse.click();
-        var elseParis = element.all(by.css(".home-tabs__more-switcher.dropdown2__switcher")).last().getText();
+        element(by.css(".home-tabs__more-switcher.dropdown2__switcher")).click();
+        var saveValueParis = element.all(by.css(".home-tabs__more-switcher.dropdown2__switcher")).last().getText();
 
-        expect(elseLondon).toEqual(elseParis);
+        expect(saveValueLondon).toEqual(saveValueParis);
 
     })
 })
