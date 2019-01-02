de85f3a61ae65070905118753d9a2f8f85f3203e
Evgeniy Beletskiy <mbgroup1988@gmail.com>
2019-01-02 16:17:35 +0300

    Fix

--- yandexpage/LocationPage.js
+++ yandexpage/LocationPage.js
@@ -1,13 +1,13 @@
 var mainPage = require('./MainPage');
 var expectation = require('C:/Users/anduser/AppData/Roaming/npm/node_modules/protractor/example/yandexelse/expectation/expectation.js');
 var inputCity = element(by.css(".input__control.input__input"));
-var popupCity = element(by.css('.popup__items.input__popup-items > li:first-child'));
 var LocationPage = function () {
     this.switchCity = function(city) {
         mainPage.goToLocationPage ();
+        var popupCity = element(by.xpath('//div[text()="'+city+'"]'));
         expectation.waitElement(inputCity);
         inputCity.clear(inputCity);
-        inputCity.sendKeys(city);
+        inputCity.clear().sendKeys(city);
         expectation.isClickable(popupCity);
         //browser.sleep("1000");
         popupCity.click(); 
